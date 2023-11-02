d128a5f90c59dd515029924ec64f644f406763a4
GitHalex <calex1731@gmail.com>
2023-11-01 22:19:58 -0400

    Ejercicios terminados de la parte 5

---
+++ Parte1/Ex5.py
@@ -0,0 +1,29 @@
+""" lado = int(input("perimeter of square: "))
+perimeter = lado*4
+print(perimeter) """
+
+""" num1 = int(input("numero 1: "))
+num2 = int(input("numero 2: "))
+num3 = int(input("numero 3: "))
+num4 = int(input("numero 4: "))
+
+suma = f"La suma de {num1}+{num2}={num1+num2}"
+producto = f"El producto de {num3}*{num4}={num3*num4}"
+print(suma)
+print(producto) """
+
+""" num1 = int(input("numero 1: "))
+num2 = int(input("numero 2: "))
+num3 = int(input("numero 3: "))
+num4 = int(input("numero 4: "))
+
+suma = num1+num2+num3+num4
+promedio = suma/4
+print(f"Promedio {promedio}")
+ """
+
+
+""" horas_trabajadas = int(input("Horas trabajadas: "))
+valor_hora = 100
+sueldo_mensual = horas_trabajadas * valor_hora
+print(f"El sueldo del empleado es: {sueldo_mensual}") """
