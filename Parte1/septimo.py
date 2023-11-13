6224a41a02e46d27e3b82aad207b01500e46365a
GitHalex <calex1731@gmail.com>
2023-11-12 23:20:13 -0400

    EL capitulo septimo terminado

---
+++ Parte1/septimo.py
@@ -0,0 +1,52 @@
+#Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de ellos.
+""" numero1 = int(input("primer numero: "))
+numero2 = int(input("segundo numero: "))
+numero3 = int(input("tercer numero: "))
+
+mayor = numero1
+
+if numero2 > mayor:
+   mayor = numero2
+if numero3 > mayor:
+   mayor =  numero3
+
+print(f"El numero mayor es {mayor}") """
+
+""" valor_entero = int(input("Ingrese un numero entero: desde 1 hasta 999 "))
+
+if valor_entero > 0 and valor_entero <= 999:
+   print(f"{valor_entero} es numero positivo")
+elif valor_entero == 0:
+   print(f"{valor_entero} es un numero nulo")
+else:
+   print(f"{valor_entero} es un numero negativo")
+ """
+
+""" numero = int(input("ingrese un numero entero de 1 o 2 o 3 cifras perri: "))
+
+if numero >= 0 and numero <= 9: print(f"{numero} el numero tiene 1 cifra")
+if numero >=10 and numero <= 99: print(f"{numero} el numero tiene 2 cifras")
+if numero >= 100 and numero <= 999: print(f"{numero} el numero tiene 3 cifras")
+if numero < 0: print(f"{numero} me introduciste un numero negativo boludo") """
+
+
+""" cantidad_preguntas = 200
+respuestas = int(input("Cantidad de respuestas correctas que contesto: "))
+porcentaje = int( (respuestas / cantidad_preguntas) *100)
+
+if porcentaje >= 90:
+   print(f"Nivel máximo:  \t  Promedio {porcentaje}")
+elif porcentaje >= 75 and porcentaje < 90:
+   print(f"Nivel medio:  \t  Promedio {porcentaje}")
+elif porcentaje >= 50 and porcentaje < 75:
+   print(f"Nivel regular:  \t  Promedio {porcentaje}")
+elif porcentaje < 50:
+   print(f"Fuera de nivel:  \t  Promedio {porcentaje}") """
+
+   
+   
+   
+
+
+
+
