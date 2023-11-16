d306fc1f3896f86b77d4b68027e080b6a6684f64
GitHalex <calex1731@gmail.com>
2023-11-15 22:42:36 -0400

    casi esta terminado

---
+++ Parte1/octavo.py
@@ -0,0 +1,56 @@
+""" print("Ingresar una fecha")
+dia = int(input("Ingrese el Dia: "))
+mes = input("Ingrese el Mes:")
+
+if dia > 0 and dia == 25 and mes.lower() == "diciembre":
+   print(f"{dia}-{mes} es NAVIDAD")
+else:
+   print(f"{dia}-{mes} cualquier dia del año") """
+
+""" num1 = int(input("primer numero: "))
+num2 = int(input("segundo numero: "))
+num3 = int(input("tercer numero: "))
+
+if num1 < 10 and num2 <10 and num3 < 10:
+   print("Todos los numeros son menores a 10") """
+
+
+""" num1 = int(input("primer numero: "))
+num2 = int(input("segundo numero: "))
+num3 = int(input("tercer numero: "))
+
+if num1 < 10 or  num2 <10 or  num3 < 10:
+   print("Alguno de los numeros es menor a diez") """
+
+""" num1 = int(input("primer numero: "))
+num2 = int(input("segundo numero: "))
+num3 = int(input("tercer numero: "))
+
+if num1 == num2 and num1 == num3:
+   suma = num1*num2
+   print(suma)
+   resultado = suma*num3
+   print(resultado) """
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
