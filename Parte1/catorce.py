8b2c839e93c557c488b82dc320d249e11c39b8ea
GitHalex <calex1731@gmail.com>
2024-06-05 17:16:51 -0400

    numero del catorce terminado

--- Parte1/catorce.py
+++ Parte1/catorce.py
@@ -1,7 +1,20 @@
-listaNumeros = [101, 20, 300, 400, 100, 200, 3, 6, 7]
+""" listaNumeros = [101, 20, 300, 400, 100, 200, 3, 6, 7]
 numerosMayor = 0
 for numero in listaNumeros:
   if numero > 100:
     numerosMayor += 1
 
-print(f"Cantidad de numeros mayores a 100 son: {numerosMayor}")
+print(f"Cantidad de numeros mayores a 100 son: {numerosMayor}") """
+
+
+""" for i in range(5):
+  numero = int(input("Ingrese un numero: "))
+  if numero >= 7:
+    print(numero) """
+
+nombres = ["Alex", "Ricardo", "Jorge", "Pijas", "Adan"]
+nombres_cinco = 0
+for nombre in nombres:
+  if len(nombre) >= 5:
+    nombres_cinco += 1
+print(nombres_cinco)
