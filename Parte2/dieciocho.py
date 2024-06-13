0ab980a09bcdf44963896da968767329099d4aac
GitHalex <calex1731@gmail.com>
2024-06-13 18:00:45 -0400

    ejercicio donde ordenamos el numero de sueldos

--- Parte2/dieciocho.py
+++ Parte2/dieciocho.py
@@ -1,4 +1,4 @@
-nombres = []
+""" nombres = []
 for x in range(5):
     nombre = input("Ingrese un nombre: ")
     nombres.append(nombre)
@@ -11,4 +11,23 @@ for i in range(len(nombres) - 1):
         if nombres[j] > nombres[j + 1]:
             nombres[j], nombres[j + 1] = nombres[j + 1], nombres[j]
 
-print(f"Lista de nombres ordenada alfabéticamente\n{nombres}")
+print(f"Lista de nombres ordenada alfabéticamente\n{nombres}") """
+
+cantidad_empleados = int(input("Ingrese la cantidad de empleados: "))
+nombres_trabajadores = []
+sueldos_trabajadores = []
+for i in range(cantidad_empleados):
+  nombre = input("Ingrese un nombre: ")
+  nombres_trabajadores.append(nombre)
+  sueldo = int(input("Ingrese su sueldo: "))
+  sueldos_trabajadores.append(sueldo)
+
+print(nombres_trabajadores)
+print(sueldos_trabajadores)
+
+for i in range(len(sueldos_trabajadores) - 1):
+  for j in range(len(sueldos_trabajadores) - 1 -i):
+    if sueldos_trabajadores[j] > sueldos_trabajadores[j + 1]:
+      sueldos_trabajadores[j], sueldos_trabajadores[j + 1] = sueldos_trabajadores[j + 1], sueldos_trabajadores[j]
+
+print(f"Lista de los sueldos ordenados: {sueldos_trabajadores}")
