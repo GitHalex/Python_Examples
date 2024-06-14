fb3096f613533e0016d8f9be0eae479536f5b66c
GitHalex <calex1731@gmail.com>
2024-06-14 11:35:22 -0400

    Ejercicio donde modificamos listas

--- Parte2/twenty.py
+++ Parte2/twenty.py
@@ -1,9 +1,19 @@
-lista=[[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
+""" lista=[[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
 
 for i in range(len(lista)):
   for j in range(len(lista[i])):
     if i == 0 and lista[i][j] > 50:
       lista[i][j] = 0
-      print(lista[i][j])
-    else: 
-      print(lista[i][j])
+
+print(lista) """
+
+
+lista=[[4,12,5,66], [14,6,25], [3,4,5,67,89,23,1], [78,56]]
+print(lista)
+
+for i in range(len(lista)):
+  for j in range(len(lista[i])):
+    if lista[i][j] > 10:
+      lista[i][j] = 0
+
+print(lista)
