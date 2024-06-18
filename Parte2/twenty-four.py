89a107c1fe42d4d5aaa6fa9acbb2202ce316f545
GitHalex <calex1731@gmail.com>
2024-06-17 22:23:36 -0400

    Ejercicio de funciones donde contamos volcaes esta terminado

---
+++ Parte2/twenty-four.py
@@ -0,0 +1,13 @@
+
+
+def cantidadVocales(palabra: str) -> int:
+    print(f"Palabra: {palabra}")
+    contar_vocales = 0
+    for char in palabra:
+        vocal = char.lower()
+        if vocal in "aeiouáéíóú":
+            contar_vocales += 1
+    return contar_vocales
+
+print(cantidadVocales("Alex López"))
+
