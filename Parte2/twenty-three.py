5f4039c86eaeb6d0e4841f9552a1f6de1d6f7d33
GitHalex <calex1731@gmail.com>
2024-06-17 22:12:09 -0400

    Ejercicios del dia 23 terminado vamos por el 24

--- Parte2/twenty-three.py
+++ Parte2/twenty-three.py
@@ -1,6 +1,6 @@
 """ Desarrollar un programa con dos funciones. La primer solicite el ingreso de un entero y muestre el cuadrado de dicho valor. La segunda que solicite la carga de dos valores y muestre el producto de los mismos. LLamar desde el bloque del programa principal a ambas funciones. """
 
-def alCuadrado() -> int:
+""" def alCuadrado() -> int:
   numero = int(input("Ingrese un numero: "))
   return numero**2
 
@@ -10,6 +10,24 @@ def producto() -> float:
   return num1*num2
 
 print(alCuadrado())
-print(producto()) 
+print(producto())  """
 
 """ Desarrollar un programa que solicite la carga de tres valores y muestre el menor. Desde el bloque principal del programa llamar 2 veces a dicha función (sin utilizar una estructura repetitiva) """
+
+def valores() -> int: 
+    numero1 = int(input("Dame un numero 1: "))
+    numero2 = int(input("Dame un numero 2: "))
+    numero3 = int(input("Dame un numero 3: "))
+
+    candidato = numero1
+
+    if numero2 < candidato:
+        candidato = numero2
+      
+    if numero3 < candidato:
+        candidato = numero3
+    
+    return candidato
+   
+print(valores())
+
