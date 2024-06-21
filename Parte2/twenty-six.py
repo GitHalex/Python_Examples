fb337aacdd8ac429dc15f63d59eebe6f0929efcd
GitHalex <calex1731@gmail.com>
2024-06-21 09:48:49 -0400

    Ejercicio donde damos una lista de numeros y nos muestra el numero del producto

--- Parte2/twenty-six.py
+++ Parte2/twenty-six.py
@@ -26,56 +26,24 @@ palabras=["enero", "febrero", "marzo", "abril", "mayo", "junio"]
 print("Palabra con mas caracteres:",mascaracteres(palabras))
 
 
-def sumarizar(lista):
-    suma=0
-    for x in range(len(lista)):
-        suma=suma+lista[x]
-    return suma
+def producto(listaNumeros: list) -> int:
+    
+    producto = 1
 
+    for numero in listaNumeros:
+        producto *= numero
 
-def mayor(lista):
-    may=lista[0]
-    for x in range(1,len(lista)):
-        if lista[x]>may:
-            may=lista[x]
-    return may
-
-
-def menor(lista):
-    men=lista[0]
-    for x in range(1,len(lista)):
-        if lista[x]<men:
-            men=lista[x]
-    return men
+    
+    return producto
     
 
-# bloque principal del programa
 
-""" listavalores=[10, 56, 23, 120, 94]
-print("La lista completa es")
-print(listavalores)
-print("La suma de todos su elementos es", sumarizar(listavalores))
-print("El mayor valor de la lista es", mayor(listavalores))
-print("El menor valor de la lista es", menor(listavalores))
+listaEnteros = []
+
+for j in range(5):
+    numero = int(input("Ingrese un numero: "))
+    listaEnteros.append(numero)
+
+print(f"el producto de la lista: {listaEnteros} es: {producto(listaEnteros)}")
 
 
-def mayormenor(lista):
-    may=lista[0]
-    men=lista[0]
-    for x in range(1,len(lista)):
-        if lista[x]>may:
-            may=lista[x]
-        else:
-            if lista[x]<men:
-                men=lista[x]
-    print("El valor mayor de la lista es", may)
-    print("El valor menor de la lista es", men)          
-
-                
-# bloque principal
-
-lista=[]
-for x in range(5):
-    valor=int(input("Ingrese valor:"))
-    lista.append(valor)
-mayormenor(lista) """
