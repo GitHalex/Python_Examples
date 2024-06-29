479398d94191eefc52446a2161d200f5d3747642
GitHalex <calex1731@gmail.com>
2024-06-29 09:54:31 -0400

    Ejericico de tuplas esta terminado

--- Parte3/thirty-one.py
+++ Parte3/thirty-one.py
@@ -1,5 +1,58 @@
-def listaNumeros() -> list:
-  listaEnteros = [int(input("Ingrese un numero: ")) for _ in range(5)]
-  return listaEnteros
+def cargarEnteros()-> list:
+   listaEnteros = [int(input("Ingrese un numero entero: ")) for _ in range(5)]
+   return listaEnteros
 
-lista = listaNumeros()
+def mayorMenor(listaEnteros: list) -> tuple:
+   
+  mayor = listaEnteros[0]
+  menor = listaEnteros[0]
+
+  for i in range(1, len(listaEnteros)):
+    if listaEnteros[i] > mayor:
+      mayor = listaEnteros[i]
+    if listaEnteros[i] < menor:
+      menor = listaEnteros[i]
+
+  return (mayor, menor)
+
+numeros = cargarEnteros()
+mayorTupla, menorTupla = mayorMenor(numeros)
+print(f"Convertido a tupla: Mayor: {mayorTupla} y Menor: {menorTupla}")
+
+
+""" def tuplas(number: int) -> tuple:
+  valor = (number, number**2)
+  return valor
+
+resultado = tuplas(10)
+print(resultado)
+print(type(resultado))
+
+def cargar_fecha():
+    dd=int(input("Ingrese numero de dia:"))
+    mm=int(input("Ingrese numero de mes:"))
+    aa=int(input("Ingrese numero de año:"))
+    return (dd,mm,aa)
+
+
+def imprimir_fecha(fecha):
+    print(fecha[0],fecha[1],fecha[2],sep="/")
+
+
+# bloque principal
+
+fecha=cargar_fecha()
+imprimir_fecha(fecha)
+
+fechatupla1=(25, 12, 2016)
+print("Imprimimos la primer tupla")
+print(fechatupla1)
+fechalista=list(fechatupla1)
+print("Imprimimos la lista que se le copio la tupla anterior")
+print(fechalista)
+fechalista[0]=31
+print("Imprimimos la lista ya modificada")
+print(fechalista)
+fechatupla2=tuple(fechalista)
+print("Imprimimos la segunda tupla que se le copio la lista")
+print(fechatupla2) """
