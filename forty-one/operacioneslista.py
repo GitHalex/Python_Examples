b0926241698c59477f37733d4f7ce8626a2363c1
GitHalex <calex1731@gmail.com>
2024-07-08 12:30:53 -0400

    ejercicios donde imortamos los modulos

--- forty-one/operacioneslista.py
+++ forty-one/operacioneslista.py
@@ -1,3 +1,4 @@
+
 def cargar():
     lista=[]
     for x in range(5):
@@ -8,7 +9,8 @@ def cargar():
 
 def imprimir_mayor(lista):
     may=lista[0]
-    for x in range(1,5):
+    #si modificamos el rango para considerar posibeles errores esta seria la mejor opcion
+    for x in range(1, len(lista)):
         if lista[x]>may:
             may=lista[x]
     print("Mayor de la lista",may)
@@ -19,3 +21,11 @@ def imprimir_suma(lista):
     for elemento in lista:
         suma=suma+elemento
     print("Suma de todos sus elementos",suma)
+
+print("Me gusta ser un modulo")
+
+if __name__ == "__main__":
+    print("Prefiero ser un modulo pero puedo hacer alugar cosas pero puedo hacer algunas pruebas para usted")
+    listaPrueba = cargar()
+    imprimir_suma(listaPrueba)
+    imprimir_mayor(listaPrueba)
--- forty-one/principal.py
+++ forty-one/principal.py
@@ -1,5 +1,12 @@
-import operacioneslista
+from operacioneslista import cargar, imprimir_mayor, imprimir_suma
 
-lista = operacioneslista.cargar()
-operacioneslista.imprimir_mayor(lista)
-operacioneslista.imprimir_suma(lista)
+lista = cargar()
+imprimir_mayor(lista)
+imprimir_suma(lista)
+
+#Las funciones del modulo operacioneslista todas la lista trabaja con 5 elementos ojo perri
+#Pro si trabajamos la definicion con el rango de tamaño puede aceptar cualquier tamaño
+listaOtra = [int(input("Ingrese un numero: ")) for _ in range(3)]
+
+imprimir_mayor(listaOtra)
+imprimir_suma(listaOtra)
