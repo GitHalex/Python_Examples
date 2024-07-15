61f93a4acf4c3d73a425e1323fbd7ff72572994b
GitHalex <calex1731@gmail.com>
2024-07-15 11:32:44 -0400

    Pila esta mas implicitamente mas tipado

--- pila.py
+++ pila.py
@@ -1,41 +1,54 @@
+from typing import List
+
+
 class Stack:
+    def __init__(self) -> None:
+        """Initialize an empty stack."""
+        self._stack_list: List[int] = []
 
-  def __init__(self) -> None:
-    self.__stack_list: list = []
+    def push(self, val: int) -> None:
+        """Push a value onto the stack."""
+        self._stack_list.append(val)
 
-  def push(self, val: int):
-    self.__stack_list.append(val)
-
-  def pop(self) -> int:
-    val = self.__stack_list[-1]
-    del self.__stack_list[-1]
-    return val
-  
+    def pop(self) -> int:
+        """Pop a value from the stack and return it. Raise an IndexError if the stack is empty."""
+        if not self._stack_list:
+            raise IndexError("pop from empty stack")
+        return self._stack_list.pop()
 
 
 class AddingStack(Stack):
+    def __init__(self) -> None:
+        """Initialize an AddingStack with a sum of zero."""
+        super().__init__()
+        self._sum = 0
 
-  def __init__(self) -> None:
-    Stack.__init__(self)
-    self.__sum = 0
+    def get_sum(self) -> int:
+        """Return the sum of all values in the stack."""
+        return self._sum
 
-  def get_sum(self) -> int:
-    return self.__sum
+    def push(self, val: int) -> None:
+        """Push a value onto the stack and add it to the sum."""
+        self._sum += val
+        super().push(val)
 
-  def push(self, val: int):
-    self.__sum += val
-    Stack.push(self, val)
+    def pop(self) -> int:
+        """Pop a value from the stack and subtract it from the sum. Raise an IndexError if the stack is empty."""
+        val = super().pop()
+        self._sum -= val
+        return val
 
-  def pop(self):
-    val = Stack.pop(self)
-    self.__sum -= val
-    return val
-  
-stack_object = AddingStack()
-for i in range(5):
-  stack_object.push(i)
 
-print(stack_object.get_sum())
+if __name__ == "__main__":
+    stack_object = AddingStack()
+    for i in range(5):
+        stack_object.push(i)
 
-for j in range(5):
-  print(stack_object.pop())
+    print(f"Sum of elements in stack: {stack_object.get_sum()}")
+
+    while True:
+        try:
+            print(stack_object.pop())
+        except IndexError:
+            print("Stack is empty.")
+            break
