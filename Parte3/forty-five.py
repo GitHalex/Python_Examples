1d0303d279d8d86a149074387f8b22c7cbb00336
GitHalex <calex1731@gmail.com>
2024-07-17 15:51:29 -0400

    falta dentro la clase como modificar el telefono y el email

--- Parte3/forty-five.py
+++ Parte3/forty-five.py
@@ -1,41 +1,83 @@
-digits = [ '1111110',  	# 0
-	   '0110000',	# 1
-	   '1101101',	# 2
-	   '1111001',	# 3
-	   '0110011',	# 4
-	   '1011011',	# 5
-	   '1011111',	# 6
-	   '1110000',	# 7
-	   '1111111',	# 8
-	   '1111011',	# 9
-	   ]
+class AgendaPersonal:
+    def __init__(self):
+        """Initialize the StudentManager with empty lists for names and grades."""
+        self.names: str = []
+        self.telefonos: int = []
+        self.mails: str = []
+
+    def menu(self) -> None:
+        """Display the menu and process user choices."""
+        while True:
+            print("1- CARGA DE UN CONTACTO")
+            print("2- LISTA COMPLETA DE LA AGENDA")
+            print("3- CONSULTA EL NOMBRE DE CONTACTO")
+            print("4- MODIFICACION DE SU TELEFONO Y EMAIL")
+            print("5- FINALIZAR PROGRAMA")
+            try:
+                option = int(input("Enter your option: "))
+                if option == 1:
+                    self.add_contactos()
+                elif option == 2:
+                    self.list_contactos()
+                elif option == 3:
+                    self.list_consultaName()
+                elif option == 4:
+                    self.list_modificacion()
+                elif option == 5:
+                    print("Exiting the program.")
+                    break
+                else:
+                    print("Invalid option. Please enter a number between 1 and 4.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    def add_contactos(self) -> None:
+        """Add constactos' names and grades to the lists."""
+        num_students = int(input("How many students do you want to add? "))
+        for _ in range(num_students):
+            name = input("Enter the Contact's name: ")
+            while True:
+                try:
+                    phone = int(input("Ingrese su telefono: "))
+                    mail = input("Ingrese su correo: ")
+                    break
+                except ValueError:
+                    print("Invalid grade. Please enter a number.")
+            self.names.append(name)
+            self.telefonos.append(phone)
+            self.mails.append(mail)
+
+    def list_contactos(self) -> None:
+        """List all students with their grades."""
+        print("Complete list of contactos")
+        for name, telefono, mail in zip(self.names, self.telefonos, self.mails):
+            print(f"{name} => {telefono} => {mail}")
+        print("_____________________")
+
+    def list_consultaName(self) -> None:
+        print("Consulta de nombre: ")
+        nombreConsulta = input("Ingrese un nombre a consultar: ")
+
+        for name, telefono, mail in zip(self.names, self.telefonos, self.mails):
+            if name == nombreConsulta:
+                print("El nombre existe!!! ")
+                print(f"{name} => {telefono} => {mail}")
+            else:
+                print(f"El nombre: {nombreConsulta} no existe en la lista: {self.names}")
+    
+    def list_modificacion(self):
+        pass
+    
+
+    """def list_high_grades(self) -> None:
+        # List students with grades greater than or equal to 7.
+        print("Students with grades >= 7:")
+        for name, grade in zip(self.names, self.grades):
+            if grade >= 7:
+                print(f"{name}: {grade}")
+        print("_____________________")"""
 
 
-def print_number(num):
-	global digits
-	digs = str(num)
-	lines = [ '' for lin in range(5) ]
-	for d in digs:
-		segs = [ [' ',' ',' '] for lin in range(5) ]
-		ptrn = digits[ord(d) - ord('0')]
-		if ptrn[0] == '1':
-			segs[0][0] = segs[0][1] = segs[0][2] = '#'
-		if ptrn[1] == '1':
-			segs[0][2] = segs[1][2] = segs[2][2] = '#'
-		if ptrn[2] == '1':
-			segs[2][2] = segs[3][2] = segs[4][2] = '#'
-		if ptrn[3] == '1':
-			segs[4][0] = segs[4][1] = segs[4][2] = '#'
-		if ptrn[4] == '1':
-			segs[2][0] = segs[3][0] = segs[4][0] = '#'
-		if ptrn[5] == '1':
-			segs[0][0] = segs[1][0] = segs[2][0] = '#'
-		if ptrn[6] == '1':
-			segs[2][0] = segs[2][1] = segs[2][2] = '#'
-		for lin in range(5):
-			lines[lin] += ''.join(segs[lin]) + ' '
-	for lin in lines:
-		print(lin)
-
-
-print_number(int(input("Ingresa el número que deseas mostrar: ")))
+if __name__ == "__main__":
+    manager = AgendaPersonal()
+    manager.menu()
