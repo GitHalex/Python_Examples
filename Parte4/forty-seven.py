577302f8621fa9a666a71a837da7b62feb5ddf90
GitHalex <calex1731@gmail.com>
2024-08-19 21:01:45 -0400

    Ejercicios donde practicamos la herencia

--- Parte4/forty-seven.py
+++ Parte4/forty-seven.py
@@ -1,23 +1,49 @@
 class Persona:
-  def __init__(self, nombre: str, edad: int, nacionalidad: str) -> None:
+  def __init__(self, nombre: str, edad: int) -> None:
+    self.nombre = nombre
+    self.edad = edad
+    self.nombre_completo = f"nombre => {nombre}\nedad => {edad}"
 
-    self.nombre_completo = f"nombre => {nombre}\nedad => {edad}\nnacionalidad => {nacionalidad}"
+  @classmethod
+  def entrada_usuario(cls):
+    nombre = input("Ingrese el nombre: ")
+    edad = int(input("Ingrese su edad: "))
+    return cls(nombre, edad)
+  
   def hablar(self):
     print("Hola, estoy hablando un poco")
+  
+  def imprimir(self):
+    print(f"Nombre => {self.nombre} ")
+    print(f"Edad => {self.edad}")
 
 
 class Empleado(Persona):
-  def __init__(self, nombre: str, edad: int, nacionalidad: str, trabajo: str, salario: float) -> None:
-    super().__init__(nombre, edad, nacionalidad)
+  def __init__(self, nombre: str, edad: int, trabajo: str, salario: float) -> None:
+    super().__init__(nombre, edad)
     self.trabajo = trabajo
     self.salario = salario
 
-class Estudiante(Persona):
-  def __init__(self, nombre: str, edad: int, nacionalidad: str, notas: float, universidad: str) -> None:
-    super().__init__(nombre, edad, nacionalidad)
-    self.notas = notas
-    self.universidad = universidad
+  @classmethod
+  def entrada_trabajo_salario(cls):
+    trabajo = input("Cual es tu puesto de trabajo")
+    salario = int(input("Cual es tu sueldo: "))
+    return cls(trabajo, salario)
+  
+  def imprimir(self):
+    super().imprimir()
+    print(f"Sueldo => {self.salario}")
 
-roberto = Empleado("Roberto", 23, "boliviano", "programador", 100.4)
-roberto.hablar()
-print(roberto.salario)
+  def paga_impuestos(self):
+    if self.salario > 1000:
+      print("El empleado debe pagar impuestos")
+    else:
+      print("No paga impuestos")
+
+
+persona1 = Persona.entrada_usuario()
+persona1.imprimir()
+print("-----------------------------------------------")
+empleado1 = Empleado.entrada_trabajo_salario()
+empleado1.imprimir()
+empleado1.paga_impuestos()
