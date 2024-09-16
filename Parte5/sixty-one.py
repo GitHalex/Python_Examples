4d19101b1be0be40469836e4ac22341e505e4af5
GitHalex <calex1731@gmail.com>
2024-09-15 21:37:53 -0400

    Agregando nuevos ejercicios donde ingresamos un nuevo dato de valor

--- Parte5/sixty-one.py
+++ Parte5/sixty-one.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 class Aplicacion:
   def __init__(self) -> None:
-    self.ventana1 = tk.TK()
+    self.ventana1 = tk.Tk()
     
     self.seleccion1 = tk.IntVar()
     self.check1 = tk.Checkbutton(self.ventana1, text="Python", variable=self.seleccion1)
@@ -16,7 +16,23 @@ class Aplicacion:
     self.check3 = tk.Checkbutton(self.ventana1, text="Java", variable=self.seleccion3)
     self.check3.grid(column=0, row=2)
     
-    self.boton1 = tk.Button()
+    self.boton1 = tk.Button(self.ventana1, text="Verificar", command=self.verificar)
+    self.boton1.grid(column=0, row=4)
     
+    self.label1 = tk.Label(self.ventana1, text="cantidad")
+    self.label1.grid(column=0, row=5)
     
     self.ventana1.mainloop()
+    
+  def verificar(self):
+    cant = 0
+    if self.seleccion1.get() == 1:
+      cant += 1
+    if self.seleccion2.get() == 1:
+      cant += 1
+    if self.seleccion3.get() == 1:
+      cant += 1
+      
+    self.label1.configure(text="cantidad:"+str(cant))
+    
+aplicacion = Aplicacion()
