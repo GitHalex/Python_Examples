4df280249169a324d9f5006fe396162d8117e9f7
GitHalex <calex1731@gmail.com>
2024-09-21 11:10:45 -0400

    Agregando nuevos archivos en el registro

--- Parte5/sixty-three.py
+++ Parte5/sixty-three.py
@@ -1,4 +1,4 @@
-import tkinter as tk 
+""" import tkinter as tk 
 from tkinter import ttk
 
 
@@ -29,4 +29,68 @@ class Aplicacion:
     self.valor = self.valor - 1
     self.label1.config(text=self.valor)
     
+aplicacion = Aplicacion() """
+
+import tkinter as tk 
+from tkinter import ttk 
+
+""" class Aplicacion:
+  
+  def __init__(self) -> None:
+    self.ventana1 = tk.Tk()
+    
+    self.label1 = ttk.Label(text="Ingrese nombre de usuario: ")
+    self.label1.grid(column=0, row=0)
+    self.data1 = tk.StringVar()
+    self.entry1 = ttk.Entry(self.ventana1, width=30, textvariable=self.data1)
+    self.entry1.grid(column=1, row=0)
+    
+    self.label2 = ttk.Label(text="Ingrese clave:")
+    self.label2.grid(column=0, row=1)
+    self.data2 = tk.StringVar()
+    self.entry2 = ttk.Entry(self.ventana1, width=30, textvariable=self.data2, show="*")
+    self.entry2.grid(column=1, row=1)
+
+    self.boton1 = ttk.Button(self.ventana1, text="Ingresar", command=self.ingresar)
+    self.boton1.grid(column=1, row=2)
+    
+    self.ventana1.mainloop()
+  
+  def ingresar(self):
+    if self.data1.get() == "Alex" and self.data2.get() == "abc123":
+      self.ventana1.title("CORRECTO")
+    else:
+      self.ventana1.title("INCORRECTO")
+      
+aplicacion = Aplicacion() """
+
+
+class Aplicacion:
+  def __init__(self) -> None:
+    self.ventana1 = tk.Tk()
+    
+    self.seleccion = tk.IntVar()
+    self.seleccion.set(2)
+    
+    self.radio1 = ttk.Radiobutton(self.ventana1, text="Varon", variable=self.seleccion, value=1)
+    self.radio1.grid(column=0, row=0)
+    
+    self.radio2 = ttk.Radiobutton(self.ventana1, text="Mujer", variable=self.seleccion, value=2)
+    self.radio2.grid(column=0, row=1)
+
+    self.boton1 = ttk.Button(self.ventana1, text="Mostrar seleccionado", command=self.mostrarseleccionado)
+    self.boton1.grid(column=0, row=2)
+
+    self.label1 = ttk.Label(text="Opcion seleccionada")
+    self.label1.grid(column=0, row=3)
+
+    self.ventana1.mainloop()
+    
+  def mostrarseleccionado(self):
+    if self.seleccion.get() == 1:
+      self.label1.configure(text="Opcion seleccionado: Varon")
+    if self.seleccion.get() == 2:
+      self.label1.configure(text="Opcion seleccionado: Mujer")
+
+
 aplicacion = Aplicacion()
