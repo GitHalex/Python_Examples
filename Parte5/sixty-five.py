52f1a3f59f3b87d40560e8551f3d25901142f573
GitHalex <calex1731@gmail.com>
2024-09-22 19:39:00 -0400

    Agregando nuevos valores a nuestros cuadros

--- Parte5/sixty-five.py
+++ Parte5/sixty-five.py
@@ -3,10 +3,53 @@ import tkinter as tk
 class Aplicacion:
   def __init__(self) -> None:
     self.ventana1 = tk.Tk()
+    self.ventana1.title("Ventanas y tamaño")
     
     menubar1 = tk.Menu(self.ventana1)
     
     self.ventana1.config(menu=menubar1)
 
-    opciones1 = tk.Menu(menubar1)
-    opciones1.add_command(label="Rojo")
+    opciones1 = tk.Menu(menubar1, tearoff=0)
+    opciones1.add_command(label="Rojo", command=self.fijarrojo)
+    opciones1.add_command(label="Verde", command=self.fijarverde) 
+    opciones1.add_separator()
+    opciones1.add_command(label="Azul", command=self.fijarazul)
+    
+    self.ventana1.bind_all("<Control-r>", self.cambiar)
+    self.ventana1.bind_all("<Control-v>", self.cambiar)
+    self.ventana1.bind_all("<Control-a>", self.cambiar)
+
+    menubar1.add_cascade(label="Colores", menu=opciones1)
+
+    opciones2 = tk.Menu(menubar1, tearoff=0)
+    opciones2.add_command(label="640x480", command=self.ventanachica)
+    opciones2.add_command(label="1024x800", command=self.ventanagrande)
+
+    menubar1.add_cascade(label="Tamaños", menu=opciones2)
+
+    self.ventana1.mainloop()
+    
+  def cambiar(self, event):
+    if event.keysym == "r":
+      self.fijarrojo()
+    if event.keysym == "v":
+      self.fijarverde()
+    if event.keysym == "a":
+      self.fijarazul()
+    
+  def fijarrojo(self):
+    self.ventana1.configure(background="red")
+
+  def fijarverde(self):
+    self.ventana1.configure(background="green")
+
+  def fijarazul(self):
+    self.ventana1.configure(background="blue")
+
+  def ventanachica(self):
+    self.ventana1.geometry("640x480")
+    
+  def ventanagrande(self):
+    self.ventana1.geometry("1024x800")
+
+aplicacion = Aplicacion()
