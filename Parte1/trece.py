79caa545a05edca95a7d1ca228ca7b9d123789f3
GitHalex <calex1731@gmail.com>
2026-05-17 22:28:25 -0400

    Addes new files with examples

--- Parte1/trece.py
+++ Parte1/trece.py
@@ -1,22 +1,22 @@
-""" frase = input("Ingrese una frase: ")
+frase = input("Ingrese una frase: ")
 cadena_vacia = 0
 for char in frase:
     if char == " ":
         cadena_vacia += 1
 
-print(f"La frase tiene {cadena_vacia} espacios vacios") """
+print(f"La frase tiene {cadena_vacia} espacios vacios")
 
-""" frase = input("Ingrese una frase: ")
+frase = input("Ingrese una frase: ")
 cant_vocales = 0
 frase_min = frase.lower()
 for i in frase_min:
     if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
         cant_vocales += 1
-print(f"cantidad de vocales: {cant_vocales}") """
+print(f"cantidad de vocales: {cant_vocales}")
 
-""" clave = input("Ingrese una clave: ")
+clave = input("Ingrese una clave: ")
 tam_clave = len(clave)
 if tam_clave >= 10 and tam_clave <= 20:
     print("Contraseña Correcta")
 else:
-    print("Error") """
+    print("Error") 
